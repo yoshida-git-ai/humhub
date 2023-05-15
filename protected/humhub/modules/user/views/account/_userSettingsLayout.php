d0037adaf0cb399bd7b4c20d2208ab1a821b1de9
Yoshida <94094919+yoshida-git-ai@users.noreply.github.com>
2023-05-15 14:36:49 +0900

    Update _userSettingsLayout.php
    Japanese Language

--- protected/humhub/modules/user/views/account/_userSettingsLayout.php
+++ protected/humhub/modules/user/views/account/_userSettingsLayout.php
@@ -1,9 +1,14 @@
+// Kurahito Yoshida Japanese Language
+
 <?php
 humhub\modules\user\widgets\AccountMenu::markAsActive(['/user/account/edit-settings']);
 ?>
 
 <div class="panel-heading">
-    <?= Yii::t('UserModule.account', '<strong>User</strong> settings'); ?> <?php echo \humhub\widgets\DataSaved::widget(); ?>
+    <?= Yii::t('UserModule.account', '<strong>Account</strong> Settings') ?> <?php echo \humhub\widgets\DataSaved::widget(); ?>
+</div>
+<div class="panel-body">
+    <?= Yii::t('UserModule.account', 'プロファイルの基本設定を定義します。 自分に合ったタグを追加し、言語とタイムゾーンを選択して、失礼なユーザーをブロックできます。') ?>
 </div>
 
 <?= humhub\modules\user\widgets\AccountSettingsMenu::widget(); ?>
